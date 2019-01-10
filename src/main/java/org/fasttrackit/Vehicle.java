1546657a8ffe787d3441b7abdc1401b007bf459d
Vlad Faur <vladvasile.faur@gmail.com>
2019-01-10 19:11:24 +0200

    studied static variables

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -2,11 +2,18 @@ package org.fasttrackit;
 
 public class Vehicle {
 
+
+    public static int vehicleCount;
+
     private String name;
     private String color;
     private double mileage;
 
 
+    public Vehicle() {
+        vehicleCount++;
+
+    }
 
     public double accelerate(double speed, double durationInHours) {
         System.out.println(name + " accelerated with " + speed + " km/h for " + durationInHours + " hours. " );
