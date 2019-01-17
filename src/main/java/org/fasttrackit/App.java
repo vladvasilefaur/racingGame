fa9b49fcfd89a50ce39bffcce78d8fccb66b2a75
Vlad Faur <vladvasile.faur@gmail.com>
2019-01-17 19:39:27 +0200

    studied arrays

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,94 +1,97 @@
 package org.fasttrackit;
 
 
-import org.fasttrackit.cheater.UFO;
-
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
-        Engine firstEngine = new Engine();
-        firstEngine.manufacturer = "Renault";
-        firstEngine.capacity = 1500;
-
-
-        Car firstCar = new Car(firstEngine);
-        firstCar.setName("Dacia");
-        firstCar.doorCount = 4;
-        firstCar.setColor("black");
-        firstCar.running = true;
-        firstCar.setMileage(7.5);
-
-        System.out.println(firstCar.toString());
-
-
-//
-//        System.out.println(firstCar.getName());
-//
-//        firstCar.engine = firstEngine;
-//        firstEngine.expiryDate = LocalDateTime.now();
-//
-//
-//        double firstCarTraveledDistance = firstCar.accelerate(130, 2);
-//
-//
-//
-//
-//
-//
-//        Engine secondEngine = new Engine();
-//        secondEngine.manufacturer = "BMW";
-//        secondEngine.capacity = 4000;
-//
-//
-//       Car secondCar= new Car(secondEngine);
-//       secondCar.setName("BMW");
-//       secondCar.doorCount = 4;
-//       secondCar.setColor("red");
-//       secondCar.running = true;
-//       secondCar.setMileage(9.5);
-//
-//
-//       secondCar.engine = secondEngine;
-
-//      Example of static variables unique for the whole application
-
-        Vehicle vehicle1 = new Vehicle();
-        vehicle1.vehicleCount++;
-
-        Vehicle vehicle2 = new Vehicle();
-        vehicle2.vehicleCount = 2;
-        Vehicle.vehicleCount = 3;
-
-        System.out.println("Value from vehicle 1: " + vehicle1.vehicleCount);
-        System.out.println("Value from vehicle 2: " + vehicle2.vehicleCount);
-        System.out.println("Value from Vehicle class: " + Vehicle.vehicleCount);
-
-        new AutoVehicle();
-
-        new  AutoVehicle(new Engine());
-
-        vehicle1.accelerate(34, 12);
-
-
-        new UFO().accelerate(100,2);
-
-//   Polymorphism (an object can take multiple forms)
-
-        Vehicle ufo = new UFO();
-        ufo.accelerate(200,2);
-
-//   The type of the variable determines what methods can be invoked
-//   We might need type casting (see below)
-
-
-        ((UFO) ufo).concealCheating();
-
-
+        Game game = new Game();
+        game.start();
     }
+//    {
+
+
 }
+//
+//        Engine firstEngine = new Engine();
+//        firstEngine.manufacturer = "Renault";
+//        firstEngine.capacity = 1500;
+//
+//
+//        Car firstCar = new Car(firstEngine);
+//        firstCar.setName("Dacia");
+//        firstCar.doorCount = 4;
+//        firstCar.setColor("black");
+//        firstCar.running = true;
+//        firstCar.setMileage(7.5);
+//
+//        System.out.println(firstCar.toString());
+//
+//
+////
+////        System.out.println(firstCar.getName());
+////
+////        firstCar.engine = firstEngine;
+////        firstEngine.expiryDate = LocalDateTime.now();
+////
+////
+////        double firstCarTraveledDistance = firstCar.accelerate(130, 2);
+////
+////
+////
+////
+////
+////
+////        Engine secondEngine = new Engine();
+////        secondEngine.manufacturer = "BMW";
+////        secondEngine.capacity = 4000;
+////
+////
+////       Car secondCar= new Car(secondEngine);
+////       secondCar.setName("BMW");
+////       secondCar.doorCount = 4;
+////       secondCar.setColor("red");
+////       secondCar.running = true;
+////       secondCar.setMileage(9.5);
+////
+////
+////       secondCar.engine = secondEngine;
+//
+////      Example of static variables unique for the whole application
+//
+//        Vehicle vehicle1 = new Vehicle();
+//        vehicle1.vehicleCount++;
+//
+//        Vehicle vehicle2 = new Vehicle();
+//        vehicle2.vehicleCount = 2;
+//        Vehicle.vehicleCount = 3;
+//
+//        System.out.println("Value from vehicle 1: " + vehicle1.vehicleCount);
+//        System.out.println("Value from vehicle 2: " + vehicle2.vehicleCount);
+//        System.out.println("Value from Vehicle class: " + Vehicle.vehicleCount);
+//
+//        new AutoVehicle();
+//
+//        new  AutoVehicle(new Engine());
+//
+//        vehicle1.accelerate(34, 12);
+//
+//
+//        new UFO().accelerate(100,2);
+//
+////   Polymorphism (an object can take multiple forms)
+//
+//        Vehicle ufo = new UFO();
+//        ufo.accelerate(200,2);
+//
+////   The type of the variable determines what methods can be invoked
+////   We might need type casting (see below)
+//
+//
+//        ((UFO) ufo).concealCheating();
+//
+//
+//    }
+
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -3,4 +3,34 @@ package org.fasttrackit;
 public class Game {
 
 
+    private Track[] tracks = new Track[10];
+
+
+    Vehicle firstCompetitor;
+    Vehicle secondCompetitor;
+
+    public void start() {
+        addTracks();
+        displayAvailableTracks();
+
+    }
+
+    private void addTracks() {
+        Track track1 = new Track("Highway", 300);
+        Track track2 = new Track("Seaside", 100);
+        tracks[0] = track1;
+        tracks[1] = track2;
+    }
+
+    private void displayAvailableTracks() {
+        System.out.println("Available tracks:");
+        System.out.println(tracks[0].getName());
+        System.out.println(tracks[1].getName());
+    }
+
 }
+
+
+
+
+
